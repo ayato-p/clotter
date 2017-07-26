e10383dd52216a8ad01cb3b26fd2a882cfccb354
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-07-26 19:23:17 +0900

    :)

--- src/cljs/clotter/input_field.cljs
+++ src/cljs/clotter/input_field.cljs
@@ -1,14 +1,20 @@
 (ns clotter.input-field
-  (:require [reagent.core :as r :refer [atom]]))
+  (:require [reagent.core :as r :refer [atom]]
+            [clojure.string :as s]))
 
 (enable-console-print!)
 
 (def open-flg (atom false))
+(def content (atom ""))
 
 (defn input-field []
   [:div#inputField
-   [:textarea {:placeholder "Hoody!" :on-focus #(reset! open-flg true) :on-blur #(reset! open-flg false)}]
-   [:div.actionArea {:class (if @open-flg "input-open" "input-close")}
+   [:textarea {:placeholder "What's happening?"
+               :value @content
+               :on-focus #(reset! open-flg true)
+               :on-blur #(reset! open-flg false)
+               :on-change #(reset! content (-> % .-target .-value))}]
+   [:div.actionArea {:class (if (or @open-flg (not (s/blank? @content))) "input-open" "input-close")}
     [:input.file {:type "file"}]
     [:p.count "10"]
     [:input.btn.btn-primary {:type "button" :value "Tweet" :on-click #(js/alert "hogehoge")}]]])
