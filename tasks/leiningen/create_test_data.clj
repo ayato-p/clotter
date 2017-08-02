d9ee267d2f8d7cbcdd762362b9422484173c9e1a
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-08-03 00:27:23 +0900

    add test data sql

--- tasks/leiningen/create_test_data.clj
+++ tasks/leiningen/create_test_data.clj
@@ -16,18 +16,18 @@
             (->User "guido" "s.tadokoro0317+t7@gmail.com" "NML" "2017-07-29 10:00:00" "5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8" true)
             (->User "ken" "s.tadokoro0317+t8@gmail.com" "NML" "2017-06-20 10:00:00" "5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8" true)
             (->User "yukihiro" "s.tadokoro0317+t9@gmail.com" "NML" "2017-07-29 10:00:00" "5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8" false)
-            (->User "hogehoge" "s.tadokoro0317+t10@gmail.com" "NML" "2017-07-29 10:00:00" "5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8" true)])o
+            (->User "hogehoge" "s.tadokoro0317+t10@gmail.com" "NML" "2017-07-29 10:00:00" "5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8" true)])
 
 (defn create-user-sql [users]
-  (map (fn [u]
-         (str "INSERT INTO user (id, email, auth_type, last_login, hashed_pass, is_acrive) VALUES "
-              "('" (:id u) "', '" (:email u) "', '" (:auth_type u) "', '" (:last_login u) "', '"
-              (:hashed_pass u) "', '" (:is_active u) "');"))
-       users))
+  (s/join "\n" (map (fn [u]
+                      (str "INSERT INTO user (id, email, auth_type, last_login, hashed_pass, is_acrive) VALUES "
+                           "('" (:id u) "', '" (:email u) "', '" (:auth_type u) "', '" (:last_login u) "', '"
+                           (:hashed_pass u) "', '" (:is_active u) "');"))
+                    users)))
 
 ;; follow --------------------------------------------------
 
-(defn- list-follows []
+(defn- list-follows [users]
   (let [ids (map :id users)]
     (map (fn [id]
            {:user-id id
@@ -43,9 +43,9 @@
        follows)))
 
 (defn- create-follows-sql [follows]
-  (map (fn [follow]
-         (str "INSERT INTO follow (from_id, to_id) VALUES "
-              "('" (:from follow) "', '" (:to follow) "');")) follows))
+  (s/join "\n" (map (fn [follow]
+                      (str "INSERT INTO follow (from_id, to_id) VALUES "
+                           "('" (:from follow) "', '" (:to follow) "');")) follows)))
 
 ;; tweet --------------------------------------------------
 
@@ -57,19 +57,28 @@
                    "デビュー" "サクセス" "ガンギマリ" "爆速" "カリスマ" "カーニバル" "おいしい" "女子大生" "考古学" "助けて" "大炎上" "特殊部隊" "仙人" "暴動"
                    "管理人" "先生" "ぶっちゃけ" "極上" "ダイナマイト" "ユビキタス" "健康法" "決定版" "謝罪会見" "キムチチゲ" "ベリーマッチ" "音速" "チルドレン"
                    "ホラーショー" "めっちゃ" "ジェントルメン" "御一行様" "行くぞ!" "たまらねえ" "大好き☆" "特攻野郎" "おじいちゃん" "愚連隊"
-                   "鉄道" "世界遺産" "ゴールデン" "ハイパー" "伝説" "最強" "銀河系" "やめろ！" "激おこ"])
+                   "鉄道" "世界遺産" "ゴールデン" "ハイパー" "伝説" "最強" "銀河系" "やめろ！" "激おこ" "やっぱりやめる"
+                   "悲しみの" "いけない" "ルージュマジック" "退廃的" "上から下まで" "まさかとは思ったが" "がいいなあ…"
+                   " https://docs.oracle.com/javase/jp/8/docs/api/ " " http://qiita.com/hatobus/items/69b353dea3d8f2a25441 "])
 
-(defn create-tweet [phrases n]
-  (take n (repeatedly
-           (fn [] (s/join (take (+ (rand-int 2) 2)
-                                (repeatedly #(rand-nth phrases))))))))
+(def tweet-size 10000)
 
-(defn create-tweet-sql [tweets]
-  (map (fn [t]
+(defn create-tweet-sql [phrases tweet-size]
+  (s/join "\n" (map (fn [t]
          (str "INSERT INTO tweet (user_id, content) VALUES "
-              "(" (rand-nth (map :id users)) "," t  ")"))
-       tweets))
+              "('" (rand-nth (map :id users)) "', '" t "');"))
+                    (take tweet-size (repeatedly
+                                      (fn [] (s/join (take (+ (rand-int 10) 2)
+                                                           (repeatedly #(rand-nth phrases))))))))))
+;; favorite
+
+(defn create-favorite [users tweet-size]
+  (s/join "\n"
+          (take tweet-size
+                (repeatedly (fn []
+                              (str "INSERT INTO favorite (user_id, tweet_id) VALUES "
+                                   "('" (rand-nth (map :id users)) "', " (rand-int tweet-size) ");"))))))
 
 (defn create-test-data [project]
   ;; user
-  (println users))
+ )
